5d7aa935420bef1e652f327b7d5d9f4049422061
john.anderson <john.anderson@gimo.co.uk>
2018-06-01 17:15:23 +0100

    First commit.

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Soaring Penguin Press</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css" />
+<!--    <script src="main.js"></script>-->
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="login">
+                <a href="">Login</a>
+            </div>
+            <div class="geolocation">
+                <select name="location">
+                    <option value="">Location 1</option>
+                    <option value="">Location 2</option>
+                </select>
+                <select name="currency">
+                    <option value="">Currency 1</option>
+                    <option value="">Currency 2</option>
+                </select>   
+            </div>
+            <div class="cart">
+                <a href="">Cart</a>
+            <div>
+            <nav class="mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <div class="featured carousel">
+            </div>
+            <div class="bestseller carousel">
+            </div>
+        </main>
+        <footer>
+        </footer>
+    </div>
+    
+</body>
+</html>
